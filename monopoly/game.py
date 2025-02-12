8a0de058bf8f4b598b8ff7db0c081fd900eb28b9
toasterbirb <git@toasterbirb.com>
2025-02-12 19:01:26 +0000

    Change minimum auction bid to 1

--- monopoly/game.py
+++ monopoly/game.py
@@ -867,7 +867,7 @@ class MonopolyGame():
 						m.author.id in self.uid
 						and self.bal[self.uid.index(m.author.id)] >= int(m.content)
 						and self.isalive[self.uid.index(m.author.id)]
-						and int(m.content) >= 0
+						and int(m.content) >= 1
 					)
 				return (
 					m.author.id in self.uid
